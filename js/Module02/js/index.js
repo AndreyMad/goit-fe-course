5e2e82df5c47c8e75f965949078bfd0098ee3db6
andreymad <andrey.mad.87@gmail.com>
2019-04-25 00:30:21 +0300

    module02 task1

--- js/Module02/js/index.js
+++ js/Module02/js/index.js
@@ -1,6 +1,19 @@
 let input;
 const numbers = [];
 let total = 0;
-for(input !== null){
-  
+
+while (true) {
+  input = prompt("Enter number");
+  if (input === null) {
+    break;
+  } else if (isNaN(input)) {
+    alert("Its not a number!");
+    continue;
+  } else {
+    numbers.push(Number(input));
+  }
 }
+for (let element of numbers) {
+  total += element;
+}
+console.log(total);
