1640c42c3cc07e75d345e2753a73807748fbea7e
Andrey Madzhuha <Andrey.mad.87@gmail.com>
2019-04-26 12:33:30 +0300

    Module02 attempt2

--- js/Module02/js/index.js
+++ js/Module02/js/index.js
@@ -1,41 +1,44 @@
 // task1
-// let input;
-// const numbers = [];
-// let total = 0;
+let input;
+const numbers = [];
+let total = 0;
 
-// while (true) {
-//   input = prompt("Enter number");
-//   if (input === null) {
-//     break;
-//   } else if (isNaN(input)) {
-//     alert("Its not a number!");
-//     continue;
-//   } else {
-//     numbers.push(Number(input));
-//   }
-// }
-// for (let element of numbers) {
-//   total += element;
-// }
-// console.log(total);
+while (true) {
+  input = prompt("Enter number");
+  if (input === null) {
+    break;
+  } else if (Number.isNaN(Number(input))) {
+    alert("Its not a number!");
+    continue;
+  } else {
+    numbers.push(Number(input));
+  }
+}
+if(numbers.length){
+for (let element of numbers) {
+  total += element;
+}} else{
+    alert("Нou have not entered any numbers");
+}
+
+console.log(total);
 
 //task2
 
 const passwords = ["qwerty", "111qwe", "123123", "r4nd0mp4zzw0rd"];
 let attemptsLeft = 3;
-while (attemptsLeft > 0) {
+while (attemptsLeft) {
   let userPassword = prompt("Enter your password:");
   attemptsLeft -= 1;
   if (userPassword === null) {
     alert("Canceled");
     break;
-  }else if (passwords.includes(userPassword)) {
+  }if (passwords.includes(userPassword)) {
     alert("Добро пожаловать!");
     break;
-  } else if (attemptsLeft === 0) {
+  }if (attemptsLeft === 0) {
     alert("У вас закончились попытки, аккаунт заблокирован!");
     break;
-  } else {
-    alert(`Неверный пароль, у вас осталось ${attemptsLeft} попыток`);
-  }
+  } alert(`Неверный пароль, у вас осталось ${attemptsLeft} попыток`);
+
 }
