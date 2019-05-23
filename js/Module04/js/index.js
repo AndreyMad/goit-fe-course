471acf8d06f4918d614671c7e638df9f345d0598
Andrey Madzhuha <Andrey.mad.87@gmail.com>
2019-05-23 17:15:43 +0300

    module 04 start

---
+++ js/Module04/js/index.js
@@ -0,0 +1,224 @@
+//Создай объект notepad для работы с массивом заметок. Каждая заметка это объект следующего формата:
+
+// const id: string | integer;
+// const title: string;
+// const body: string;
+// const priority: integer [0-2];
+//id - уникальный идентификатор объекта, чтобы можно было найти его в коллекции, строка.
+// //title - заголовок заметки, строка.
+// body - текст заметки, строка.
+// priority - значение приоритета, от 0 (низкий) до 2 (высокий).
+// Вот карта приоритетов, используй ее.
+
+const Priority = {
+  LOW: 0,
+  NORMAL: 1,
+  HIGH: 2
+};
+
+const notepad = {
+  notes: [],
+  getNotes() {
+    return notepad.notes;
+  },
+  findNoteById(id) {
+    for (let element of notepad.notes) {
+      if (id === element.id) {
+        return element;
+      }
+    }
+  },
+  saveNote(note) {
+    notepad.notes.push(note);
+    return notepad.notes;
+  },
+  deleteNote(id) {
+    let indexToDelete = this.notes.indexOf(this.findNoteById(id));
+    this.notes.splice(indexToDelete, 1);
+  },
+  // updateNoteContent(id, updatedContent) {
+  //   for(let element of notepad.notes){
+  //     if(id === element.id){
+
+  //     }
+  //   }
+
+  /*
+   * Обновляет контент заметки
+   * updatedContent - объект с полями вида {имя: значение, имя: значение}
+   * Свойств в объекте updatedContent может быть произвольное количество
+   *
+   * Принимает: идентификатор заметки и объект, полями которого надо обновить заметку
+   * Возвращает: обновленную заметку
+   */
+  //},
+  updateNotePriority(id, priority) {
+    for (let element of notepad.notes) {
+      if (id === element.id) {
+        element.priority = priority;
+      }
+    }
+
+    /*
+     * Обновляет приоритет заметки
+     *
+     * Принимает: идентификатор заметки и ее новый приоритет
+     * Возвращает: обновленную заметку
+     */
+  },
+  //ilterNotesByQuery(query) {
+
+  /*
+   * Фильтрует массив заметок по подстроке query.
+   * Если значение query есть в заголовке или теле заметки - она подходит
+   *
+   * Принимает: подстроку для поиска в title и body заметки
+   * Возвращает: новый массив заметок, контент которых содержит подстроку
+   */
+  // },
+  filterNotesByPriority(priority) {
+    let newArr = [];
+    for (let element of this.notes) {
+      if (priority === element.priority) {
+        newArr.push(element);
+      }
+    }
+    return newArr;
+    // filterNotesByPriority(priority) {
+    //   const priorityNotes = [];
+    //   for (let el of this.notes) {
+    //     if (priority === el.priority) priorityNotes.push(el);
+    //   }
+    //   return priorityNotes;
+
+    /*
+  //    * Фильтрует массив заметок по значению приоритета
+  //    * Если значение priority совпадает с приоритетом заметки - она подходит
+  //    *
+  //    * Принимает: приоритет для поиска в свойстве priority заметки
+  //    * Возвращает: новый массив заметок с подходящим приоритетом
+  //    */
+    // },
+  }
+};
+//Далее идет код для проверки работоспособности объекта, вставь его в конец скрипта. Твоя реализация методов объекта notepad должна проходить этот тест.
+
+/*
+ * Добавляю 4 заметки и смотрю что получилось
+ */
+notepad.saveNote({
+  id: "id-1",
+  title: "JavaScript essentials",
+  body:
+    "Get comfortable with all basic JavaScript concepts: variables, loops, arrays, branching, objects, functions, scopes, prototypes etc",
+  priority: Priority.HIGH
+});
+
+notepad.saveNote({
+  id: "id-2",
+  title: "Refresh HTML and CSS",
+  body:
+    "Need to refresh HTML and CSS concepts, after learning some JavaScript. Maybe get to know CSS Grid and PostCSS, they seem to be trending.",
+  priority: Priority.NORMAL
+});
+
+notepad.saveNote({
+  id: "id-3",
+  title: "Get comfy with Frontend frameworks",
+  body:
+    "First must get some general knowledge about frameworks, then maybe try each one for a week or so. Need to choose between React, Vue and Angular, by reading articles and watching videos.",
+  priority: Priority.NORMAL
+});
+
+notepad.saveNote({
+  id: "id-4",
+  title: "Winter clothes",
+  body:
+    "Winter is coming! Need some really warm clothes: shoes, sweater, hat, jacket, scarf etc. Maybe should get a set of sportwear as well so I'll be able to do some excercises in the park.",
+  priority: Priority.LOW
+});
+
+console.log("Все текущие заметки: ", notepad.getNotes());
+
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * Зима уже близко, пора поднять приоритет на покупку одежды
+ */
+// notepad.updateNotePriority('id-4', Priority.NORMAL);
+
+// console.log(
+//   'Заметки после обновления приоритета для id-4: ',
+//   notepad.getNotes(),
+// );
+
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * Решил что фреймворки отложу немного, понижаю приоритет
+ */
+// notepad.updateNotePriority('id-3', Priority.LOW);
+
+// console.log(
+//   'Заметки после обновления приоритета для id-3: ',
+//   notepad.getNotes(),
+// );
+
+// /*
+//  * Решил отфильтровать заметки по слову html
+//  */
+// console.log(
+//   'Отфильтровали заметки по ключевому слову "html": ',
+//   notepad.filterNotesByQuery('html'),
+// );
+
+// /*
+//  * Решил отфильтровать заметки по слову javascript
+//  */
+// console.log(
+//   'Отфильтровали заметки по ключевому слову "javascript": ',
+//   notepad.filterNotesByQuery('javascript'),
+// );
+
+// /*
+//  * Хочу посмотреть только заметки с нормальным приоритетом
+//  */
+console.log(
+  "Отфильтровали заметки по нормальному приоритету: ",
+  notepad.filterNotesByPriority(Priority.NORMAL)
+);
+
+// /*
+//  * Обновим контент заметки с id-3
+//  */
+// notepad.updateNoteContent('id-3', {
+//   title: 'Get comfy with React.js or Vue.js',
+// });
+
+// console.log(
+//   'Заметки после обновления контента заметки с id-3: ',
+//   notepad.getNotes(),
+// );
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// notepad.deleteNote('id-2');
+// console.log('Заметки после удаления с id -2: ', notepad.getNotes());
+
+// const users = [{
+//   id: 1,
+//   name: 'Vasya',
+//   active: true,
+// },
+// {
+//   id: 2,
+//   name: 'Igor',
+//   active: false,
+// }];
+// const updateActives = (arr, id)=> {
+//   for(let i =0; i<arr.length; i++){
+//     if(arr[i].id === id){
+//       arr[i]={
+//         ...arr[i],
+//         active: !arr[i].active,
+//       }
+//     }
+//   }
+//   return arr;
+// }
+// console.log(updateActives(users, 2));
