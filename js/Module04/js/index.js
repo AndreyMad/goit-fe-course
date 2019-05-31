90804e74bd7849ed24a7e11a5c7713886d71b5dd
Andrey Madzhuha <Andrey.mad.87@gmail.com>
2019-05-31 12:26:59 +0300

    module 04 done

--- js/Module04/js/index.js
+++ js/Module04/js/index.js
@@ -25,37 +25,29 @@ const notepad = {
     let indexToDelete = this.notes.indexOf(this.findNoteById(id));
     this.notes.splice(indexToDelete, 1);
   },
-
-
-
-
-
-
-
-
-
-
-
   updateNoteContent(id, updatedContent) {
+    for(let element of this.notes){
+      if(element.id === id){
+        Object.assign(element, updatedContent);
+        return element;
+      }
+    }
+   
+    
+
     // const updatedNote = {...this.findNoteById(id), ...updatedContent};
     // this.notes[this.notes.indexOf(this.findNoteById(id))] = updatedNote;
-    const newel ={...this.notes[this.notes.indexOf(this.findNoteById(id))], ...updatedContent};
-    return  newel;
-
-  //const {title, body} = this.findNoteById(id);
-},
-
-
-
-
-
-
-
+    // return updatedNote;
+  },
 
 
 
   updateNotePriority(id, priority) {
-    this.findNoteById(id).priority = priority;
+    for (let element of this.notes) {
+      if (id === element.id) {
+        element.priority = priority;
+      }
+    }
   },
 
   filterNotesByQuery(query) {
@@ -86,40 +78,45 @@ const notepad = {
 notepad.saveNote({
   id: "id-1",
   title: "JavaScript essentials",
-  body: "Get comfortable with all basic JavaScript concepts: variables, loops, arrays, branching, objects, functions, scopes, prototypes etc",
+  body:
+    "Get comfortable with all basic JavaScript concepts: variables, loops, arrays, branching, objects, functions, scopes, prototypes etc",
   priority: Priority.HIGH
 });
 
 notepad.saveNote({
   id: "id-2",
   title: "Refresh HTML and CSS",
-  body: "Need to refresh HTML and CSS concepts, after learning some JavaScript. Maybe get to know CSS Grid and PostCSS, they seem to be trending.",
+  body:
+    "Need to refresh HTML and CSS concepts, after learning some JavaScript. Maybe get to know CSS Grid and PostCSS, they seem to be trending.",
   priority: Priority.NORMAL
 });
 
 notepad.saveNote({
   id: "id-3",
   title: "Get comfy with Frontend frameworks",
-  body: "First must get some general knowledge about frameworks, then maybe try each one for a week or so. Need to choose between React, Vue and Angular, by reading articles and watching videos.",
+  body:
+    "First must get some general knowledge about frameworks, then maybe try each one for a week or so. Need to choose between React, Vue and Angular, by reading articles and watching videos.",
   priority: Priority.NORMAL
 });
 
 notepad.saveNote({
   id: "id-4",
   title: "Winter clothes",
-  body: "Winter is coming! Need some really warm clothes: shoes, sweater, hat, jacket, scarf etc. Maybe should get a set of sportwear as well so I'll be able to do some excercises in the park.",
+  body:
+    "Winter is coming! Need some really warm clothes: shoes, sweater, hat, jacket, scarf etc. Maybe should get a set of sportwear as well so I'll be able to do some excercises in the park.",
   priority: Priority.LOW
 });
 
 console.log("Все текущие заметки: ", notepad.getNotes());
 
-// notepad.updateNotePriority('id-4', Priority.NORMAL); 
+// notepad.updateNotePriority('id-4', Priority.NORMAL);
+
 // console.log(
 //   'Заметки после обновления приоритета для id-4: ',
 //   notepad.getNotes(),
 // );
 
-//notepad.updateNotePriority('id-3', Priority.LOW);
+// notepad.updateNotePriority('id-3', Priority.LOW);
 
 // console.log(
 //   'Заметки после обновления приоритета для id-3: ',
@@ -141,9 +138,10 @@ console.log("Все текущие заметки: ", notepad.getNotes());
 //   notepad.filterNotesByPriority(Priority.NORMAL)
 // );
 
-notepad.updateNoteContent('id-4', {
-  title: 'asdasdasdasdasdasdasdasd',
+notepad.updateNoteContent('id-3', {
+  title: 'Get comfy with React.js or Vue.js',
 });
+console.log(notepad.getNotes());
 // console.log(
 //   'Заметки после обновления контента заметки с id-3: ',
 //   notepad.getNotes(),
