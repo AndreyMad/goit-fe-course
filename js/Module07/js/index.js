df57b68d5d0408869b35e11cee0d9754477b07fa
andreymad <andrey.mad.87@gmail.com>
2019-06-19 21:14:35 +0300

    little trouble

--- js/Module07/js/index.js
+++ js/Module07/js/index.js
@@ -145,7 +145,12 @@ console.log(getTotalBalance(users)); // 20916
 // 1.8. Задание 8
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 
-const getUsersByFriend = (users, name) => users.filter(user=>user.friends.find(userName => userName === name));
+const getUsersByFriend = (users, name) => users.filter(user=>user.friends
+  .find(userFriendsName => userFriendsName === name)).map(user=>user.name);
+
+
+//const getUsersByFriend =(users, name)=> users.filter(user=>user.friends.some(el=>el===name)).map(user=>user.name);
+
 console.log(getUsersByFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
  console.log(getUsersByFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
