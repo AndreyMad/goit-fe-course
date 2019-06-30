4e89a5f8574834152905a5e66cbe043db81a4ab3
andreymad <andrey.mad.87@gmail.com>
2019-06-30 19:33:18 +0300

    module08

--- js/Module09/js/app.js
+++ js/Module09/js/app.js
@@ -117,6 +117,9 @@ const initialNotes = [{
 ////////////////////////////////////////////////////////////////////
 
 const notepad = new Notepad(initialNotes);
+// const notes = notepad.notes;
+// const noteList = document.querySelector('.note-list')
+
 
 function createNoteContent({
   title,
@@ -215,3 +218,124 @@ let root = document.querySelector('.note-list');
 
 renderNoteList(root, notepad.notes);
 
+
+
+
+
+
+// let noteList = document.qu
+// actionIcon.textContent = type;
+// erySelector('.note-list')
+// noteList.append() 
+// createNoteContent(createActionButton(initialNotes))
+
+
+
+
+
+
+
+
+
+
+
+// let fragment = [];
+//     initialNotes.forEach(element => {
+//       fragment.push(createNoteContent(element))
+//     })
+
+//     return fragment;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function createNoteContent(title, body) {
+//   let noteContent = document.createElement('div');
+//   noteContent.classList.add('note__content');
+
+//   let noteTitle = document.createElement('h2');
+//   noteTitle.classList.add('note__title');
+//   noteTitle.textContent = title;
+
+//   let noteBody = document.createElement('p');
+//   noteBody.classList.add('note__body');
+//   noteBody.textContent = body;
+
+//   noteContent.append(noteTitle);
+//   noteContent.append(noteBody);
+
+//   return noteContent;
+// }
+
+// function createActionButton(action, type) {
+//   let btn = document.createElement('button');
+//   btn.classList.add('action');
+//   btn.dataset.action = action;
+
+//   let icon = document.createElement('i');
+//   icon.classList.add('material-icons', 'action__icon');
+//   icon.textContent = type;
+
+//   btn.append(icon);
+
+//   return btn;
+// }
+
+// function createNoteFooter(priority) {
+//   let noteFooter = document.createElement('footer');
+//   noteFooter.classList.add('note__footer');
+
+//   let noteSection = document.createElement('section');
+//   noteSection.classList.add('note__section');
+
+//   let noteSection2 = noteSection.cloneNode(true);
+
+//   let notePriority = document.createElement('span');
+//   notePriority.classList.add('note__priority');
+//   notePriority.textContent = `Priority: ${priority}`
+
+// noteSection.append(createActionButton(NOTE_ACTIONS.DECREASE_PRIORITY, ICON_TYPES.ARROW_DOWN), createActionButton(NOTE_ACTIONS.INCREASE_PRIORITY, ICON_TYPES.ARROW_UP), notePriority);
+
+// noteSection2.append(createActionButton(NOTE_ACTIONS.EDIT, ICON_TYPES.EDIT), createActionButton(NOTE_ACTIONS.DELETE, ICON_TYPES.DELETE));
+
+// noteFooter.append(noteSection, noteSection2);
+
+//   return noteFooter;
+// }
+
+// function createListItem({ id, title, body, priority }) {
+
+//   let noteListItem = document.createElement('li');
+//   noteListItem.classList.add('note-list__item');
+//   noteListItem.dataset.id = id;
+
+//   let noteWrapper = document.createElement('div');
+//   noteWrapper.classList.add('note');
+
+//   noteWrapper.append(createNoteContent(title, body), createNoteFooter(priority));
+
+//   noteListItem.append(noteWrapper);
+
+//   return noteListItem;
+// }
+
+// function renderNoteList(listRef, notes) {
+//   let listNotes = notes.map(note => createListItem(note));
+//   listRef.append(...listNotes);
+// }
+
+// let rootNode = document.querySelector('.note-list');
+
+// renderNoteList(rootNode, notepad.notes);
