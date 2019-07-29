d73fafee74d0af4ff060e1ed3ab79cc44dc54471
andreymad <andrey.mad.87@gmail.com>
2019-07-29 22:54:56 +0300

    module 10

--- js/Module11/notepad-boilerplate-homework-11/src/js/utils/app.js
+++ js/Module11/notepad-boilerplate-homework-11/src/js/utils/app.js
@@ -1,17 +1,18 @@
 import initialNotes from "../../assets/notes.json";
 import { PRIORITY_TYPES } from "./constants";
 import Notepad from "./notepad-model";
-import { createListItem, renderNoteList } from "./view";
+import {refs, renderNoteList} from "./view";
 import MicroModal from "micromodal";
+import { Notyf } from "notyf";
+import 'notyf/notyf.min.css';
 
-let root = document.querySelector(".note-list");
+const notyf = new Notyf({duration:2000});
+
 let inputTitleValue = document.getElementsByName("note_title")[0];
 let inputBodyValue = document.getElementsByName("note_body")[0];
 let shortId = require("shortid");
-let modalBtn = document.querySelector(".page-header__button");
 let notepad;
-let modalForm = document.querySelector(".modal__container");
-let inputValue = document.querySelector(".search-form__input");
+
 
 if (localStorage.getItem("notes") !== null) {
   let localNotes = JSON.parse(localStorage.getItem("notes"));
@@ -19,17 +20,20 @@ if (localStorage.getItem("notes") !== null) {
 } else {
   notepad = new Notepad(initialNotes);
 }
+renderNoteList(refs.root, notepad.notes)
 
 function deleteNote({ target }) {
   if (target.parentNode.dataset.action === "delete-note") {
     notepad.deleteNote(target.closest(".note-list__item").dataset.id);
     target.closest(".note-list__item").remove();
+    notyf.success('Заметка успешно удалена!')
     pushToLocalStorage();
   }
 }
 
 function addListItem(listRef, note) {
-  listRef.append(createListItem(note));
+  listRef.insertAdjacentHTML('beforeend', note)
+  rootRefresh()
 }
 
 function submit(e) {
@@ -43,19 +47,21 @@ function submit(e) {
     });
     inputTitleValue.value = "";
     inputBodyValue.value = "";
-    addListItem(root, newNote);
+    addListItem(refs.root, newNote);
     pushToLocalStorage();
-  } else alert("Необходимо заполнить все поля!");
+    closeModal();
+    notyf.success('Заметка добалена!');
+  } else notyf.error("Необходимо заполнить все поля!");
 }
 
 function filterNotes({ target }) {
-  root.innerHTML = "";
-  renderNoteList(root, notepad.filterNotesByQuery(target.value));
+  refs.root.innerHTML = "";
+  renderNoteList(refs.root, notepad.filterNotesByQuery(target.value));
 }
 
-modalForm.addEventListener("submit", submit);
-root.addEventListener("click", deleteNote);
-inputValue.addEventListener("input", filterNotes);
+refs.modalForm.addEventListener("submit", submit);
+refs.root.addEventListener("click", deleteNote);
+refs.inputValue.addEventListener("input", filterNotes);
 //renderNoteList(root, notepad.notes);
 
 //////////////////////////////////////////
@@ -66,10 +72,11 @@ function editNote({ target }) {
     noteToEdit = notepad.findNoteById(
       target.closest(".note-list__item").dataset.id
     );
+    showModal()
     inputBodyValue.value = noteToEdit.body;
     inputTitleValue.value = noteToEdit.title;
-    modal.removeEventListener("click", submit);
-    modal.addEventListener("click", saveEdited);
+    refs.modalForm.removeEventListener("submit", submit);
+    refs.modalForm.addEventListener("submit", saveEdited);
   }
 }
 
@@ -82,9 +89,11 @@ function saveEdited(e) {
   rootRefresh();
   inputTitleValue.value = "";
   inputBodyValue.value = "";
-  modal.addEventListener("click", submit);
-  modal.removeEventListener("click", saveEdited);
+  refs.modalForm.addEventListener("submit", submit);
+  refs.modalForm.removeEventListener("submit", saveEdited);
   pushToLocalStorage();
+  closeModal();
+  notyf.success('Заметка успешно обновлена!!!')
 }
 
 function changePriority({ target }) {
@@ -94,22 +103,26 @@ function changePriority({ target }) {
   if (
     target.parentNode.dataset.action === "decrease-priority" &&
     noteToEdit.priority > 0
+   
   ) {
     noteToEdit.priority--;
+    pushToLocalStorage()
     rootRefresh();
+    
   }
   if (
     target.parentNode.dataset.action === "increase-priority" &&
     noteToEdit.priority < 2
   ) {
     noteToEdit.priority++;
-    rootRefresh();
     pushToLocalStorage();
+    rootRefresh();
+  
   }
 }
 function rootRefresh() {
-  root.innerHTML = "";
-  renderNoteList(root, notepad.notes);
+  refs.root.innerHTML = "";
+  renderNoteList(refs.root, notepad.notes);
 }
 
 let pushToLocalStorage = () => {
@@ -124,8 +137,8 @@ let closeModal = () => {
   MicroModal.close("note-editor-modal");
 };
 
-root.addEventListener("click", editNote);
-root.addEventListener("click", changePriority);
-modalBtn.addEventListener("click", showModal);
+refs.root.addEventListener("click", editNote);
+refs.root.addEventListener("click", changePriority);
+refs.modalBtn.addEventListener("click", showModal);
 
 export { initialNotes };
